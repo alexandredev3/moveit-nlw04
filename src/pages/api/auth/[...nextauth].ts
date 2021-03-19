56b7721a76e0aca1227566b8c500f4b8d16f6396
alexandredev3 <costasalexandre511@gmail.com>
2021-03-19 10:59:07 -0300

    app: updated auth route

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,11 @@
 import NextAuth, { InitOptions } from "next-auth";
 import Providers from "next-auth/providers";
 import { NowRequest, NowResponse } from "@vercel/node";
+import { GenericObject } from "next-auth/_utils";
 
-interface IAccount {
-  userId: string;
-  expires: Date;
-  sessionToken: string;
-  accessToken: string;
-  id: string;
-}
+import { connectToDatabase } from '@lib/mongodb';
+
+import { ObjectId } from "mongodb";
 
 const {
   GITHUB_CLIENT_ID,
@@ -17,35 +14,79 @@ const {
   SECRET,
 } = process.env;
 
+let username: string;
+
 const options: InitOptions = {
   providers: [
     Providers.GitHub({
       clientId: GITHUB_CLIENT_ID,
       clientSecret: GITHUB_CLIENT_SECRET,
+      scope: 'public_repo',
     }),
   ],
 
   database: MONGODB_URI,
   secret: SECRET,
 
-  debug: false,
+  jwt: {
+    secret: SECRET,
+    encryption: true,
+  },
+
+  session: {
+    jwt: true,
+    maxAge: 30 * 24 * 60 * 60,
+    updateAge: 24 * 60 * 60,
+  },
+
+  events: {
+    createUser: async (message) => {
+      const { db } = await connectToDatabase();
+
+      const usersCollection = db.collection('users');
+
+      usersCollection.updateOne(
+        { _id: new ObjectId(message.id) },
+        {
+          $set: {
+            username,
+          }
+        }
+      )
+    }
+  },
+
+  debug: true,
   callbacks: {
-    async signIn(user: any, account: IAccount, profile: any) {
+    async signIn(user: GenericObject, account: GenericObject, profile: GenericObject) {
+      username = profile.login
+
       return true;
     },
     async redirect(url: string, _: string) {
-      if (url === "/api/auth/signin") {
-        return Promise.resolve("/");
-      }
-
-      return Promise.resolve("/api/auth/signin");
+      return Promise.resolve("/");
     },
     async session(session: any, user: any) {
       session.user = user;
 
       return Promise.resolve(session);
     },
-    async jwt(token: any, profile: any) {
+    async jwt(token: GenericObject, user: GenericObject, account: GenericObject, profile: GenericObject) {
+      if (user) {
+        token = {
+          id: user.id,
+          ...user
+        }
+        token.github_profile = {
+          id: profile.id,
+          name: user.name,
+          image: user.image,
+          username: profile.login,
+          followers: profile.followers,
+          following: profile.following,
+        }
+      }
+
       return Promise.resolve(token);
     },
   },
