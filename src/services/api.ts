a29229856516a4ef08f048328ba006f36600ecd5
alexandredev3 <costasalexandre511@gmail.com>
2021-03-19 15:06:19 -0300

    app: updated api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const ENVIRONMENT = process.env.NODE_ENV;
+const API_URL = process.env.API_URL
 
 const baseURL = ENVIRONMENT === 'development' 
   ? 'http://localhost:3000/api/' 
-  : 'https://moveit-alexandredev3.vercel.app/api';
+  : API_URL;
 
 export const api = axios.create({
   baseURL,
