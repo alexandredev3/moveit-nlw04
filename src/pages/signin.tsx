45a4802a65a328d5274605de8515f0601555281e
alexandredev3 <costasalexandre511@gmail.com>
2021-03-19 11:03:39 -0300

    app: updated Signin page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { GetServerSideProps } from 'next';
 import { signIn } from 'next-auth/client';
 import { getSession } from 'next-auth/client';
@@ -13,25 +14,43 @@ import {
 
 export default function SignIn() {
   return (
-    <Container>
-      <img src="/icons/simbolo.svg" alt="Simbolo icon"/>
-      <Section>
-        <img src="/logo-full.svg" alt="Full logo"/>
-        <Form>
-          <h1>Bem-vindo</h1>
-          <Github>
-            <img src="/icons/github.svg" alt="Github logo"/>
-            <span>Faça login com seu Github para começar</span>
-          </Github>
-          <InputBlock>
-            <Button type="button" onClick={() => signIn('github')}>
-              <span>Fazer login com Github</span>
-              <img src="/icons/github.svg" alt="Seta para esquerta icon"/>
-            </Button>
-          </InputBlock>
-        </Form>
-      </Section>
-    </Container>
+    <>
+      <Head>
+        <title>Faça login com seu Github para começar</title>
+
+        <meta name="description" content="Faça login com seu Github para começar" />
+
+        <meta property="og:site_name" content="Move.it" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Move.it" />
+        <meta property="og:url" content="https://moveit-alexandredev3.vercel.app/" />
+        <meta property="og:description" content="Faça login com seu Github para começar" />
+
+        <meta property="twitter:url" content="https://moveit-alexandredev3.vercel.app/" />
+        <meta property="twitter:title" content="Move.it" />
+        <meta property="twitter:description" content="Faça login com seu Github para começar" />
+      </Head>
+      <Container>
+        <img src="/icons/simbolo.svg" alt="Simbolo icon"/>
+        <Section>
+          <img src="/logo-full.svg" alt="Full logo"/>
+          <Form>
+            <h1>Bem-vindo</h1>
+            <Github>
+              <img src="/icons/github.svg" alt="Github logo"/>
+              <span>Faça login com seu Github para começar</span>
+            </Github>
+            <InputBlock>
+              <Button type="button" onClick={() => signIn('github')}>
+                <span>Fazer login com Github</span>
+                <img src="/icons/github.svg" alt="Seta para esquerta icon"/>
+              </Button>
+            </InputBlock>
+          </Form>
+        </Section>
+      </Container>
+    </>
   );
 }
 
@@ -41,7 +60,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   if (session) {
     return {
       redirect: {
-        destination: '/',
+        destination: `/${session.user.github_profile.username}`,
         permanent: false
       }
     }
